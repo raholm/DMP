16c447e75b6f376e6352d60ffd122c74f612e50f
raholm <rasmus.holm723@gmail.com>
2017-11-02 13:35:14 +0100

    Created state classes in a more uniform manner.

--- src/snake/state.py
+++ src/snake/state.py
@@ -5,71 +5,57 @@ from src.util.math import manhattan
 
 
 class SnakeState(State):
-	def key(self):
+	def __init__(self, env):
+		self.key = tuple(cls(env).data
+						 for cls in self.data_classes)
+
+	@property
+	def data_classes(self):
 		raise NotImplementedError
 
 	def __eq__(self, other):
-		return other and other.key() == self.key()
+		return other and other.key == self.key
 
 	def __hash__(self):
-		return hash(self.key())
+		return hash(self.key)
 
 	def __repr__(self):
-		return str(self.key())
+		return str(self.key)
 
 
-class BoardState(SnakeState):
+class DimensionData(object):
 	def __init__(self, env):
-		self.board = "".join(str(cell)
-							 for row in env.board.grid
-							 for cell in row) + " " + str(env.score)
-
-	def key(self):
-		return self.board
+		self.data = env.board.rows, env.board.cols
 
 
-class SnakeAndFoodWithScoreState(SnakeState):
+class ScoreData(object):
 	def __init__(self, env):
-		self.data = (tuple((row, col)
-						   for row in range(env.board.rows)
-						   for col in range(env.board.cols)
-						   if not env.board[row, col] == SnakeCellType.Empty),
-					 env.score)
-
-	def key(self):
-		return self.data
+		self.data = env.score
 
 
-class SnakeAndFoodWithoutScoreState(SnakeState):
+class BoardData(object):
+	def __init__(self, env):
+		self.data = "".join(str(cell)
+							for row in env.board.grid
+							for cell in row)
+
+
+class SnakeFoodData(object):
 	def __init__(self, env):
 		self.data = tuple((row, col)
 						  for row in range(env.board.rows)
 						  for col in range(env.board.cols)
 						  if not env.board[row, col] == SnakeCellType.Empty)
 
-	def key(self):
-		return self.data
 
-
-class DistanceState(SnakeState):
+class DistanceData(object):
 	def __init__(self, env):
-		self.snake_head = tuple(env.snake.head)
-		self.snake_tail = tuple(env.snake.tail)
-		self.distance = manhattan(self.snake_head, env.food)
-		self.score = env.score
-
-	def key(self):
-		return self.snake_head, self.snake_tail, self.distance, self.score
+		self.data = manhattan(env.snake.head, env.food)
 
 
-class DirectionalDistanceState(SnakeState):
+class DirectionalData(object):
 	def __init__(self, env):
-		self.distance = manhattan(env.snake.head, env.food)
-		self.direction = self.__get_direction(env)
-		self.score = env.score
-
-	def key(self):
-		return self.distance, self.direction, self.score
+		self.data = self.__get_direction(env)
 
 	@staticmethod
 	def __get_direction(env):
@@ -90,3 +76,99 @@ class DirectionalDistanceState(SnakeState):
 			direction = SnakeDirection.West
 
 		return direction
+
+
+class BoardState(SnakeState):
+	@property
+	def data_classes(self):
+		return [BoardData]
+
+
+class BoardDimensionState(SnakeState):
+	@property
+	def data_classes(self):
+		return [BoardData, DimensionData]
+
+
+class BoardScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [BoardData, ScoreData]
+
+
+class BoardDimensionScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [BoardData, DimensionData, ScoreData]
+
+
+class SnakeFoodState(SnakeState):
+	@property
+	def data_classes(self):
+		return [SnakeFoodData]
+
+
+class SnakeFoodDimensionState(SnakeState):
+	@property
+	def data_classes(self):
+		return [SnakeFoodData, DimensionData]
+
+
+class SnakeFoodScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [SnakeFoodData, ScoreData]
+
+
+class SnakeFoodDimensionScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [SnakeFoodData, DimensionData, ScoreData]
+
+
+class DirectionalState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData]
+
+
+class DirectionalDimensionState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DimensionData]
+
+
+class DirectionalScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, ScoreData]
+
+
+class DirectionalDimensionScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DimensionData, ScoreData]
+
+
+class DirectionalDistanceState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DistanceData]
+
+
+class DirectionalDistanceDimensionState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DistanceData, DimensionData]
+
+
+class DirectionalDistanceScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DistanceData, ScoreData]
+
+
+class DirectionalDistanceDimensionScoreState(SnakeState):
+	@property
+	def data_classes(self):
+		return [DirectionalData, DistanceData, DimensionData, ScoreData]
